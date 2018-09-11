617c6d6f89c6ac727cfe0cc6f691b54cc4b8c03a
TranHieu956230 <tranhieu956230@gmail.com>
2018-09-11 07:44:47 +0700

    'ok'

--- StudentManagement.java
+++ StudentManagement.java
@@ -1,15 +1,17 @@
+import java.util.ArrayList;
 import java.util.Vector;
-import java.util.Arrays;
+
 @SuppressWarnings("unchecked")
 public class StudentManagement {
 
-    static Student[] students = new Student[100];
+    static ArrayList<Student> students = new ArrayList<Student>();
+
 
     public static void main(String[] args) {
         // instantiate students
-        Student st1 = new Student("Trần Đức Hiếu 1", "17021248", "K59CLC", "tranhieu956230@gmail.com");
-        Student st2 = new Student("Trần Đức Hiếu 2", "17021248", "K59CLC", "tranhieu956230@gmail.com");
-        Student st3 = new Student("Trần Đức Hiếu 3", "17021248", "K59CB", "tranhieu956230@gmail.com");
+        Student st1 = new Student("Trần Đức Hiếu", "17021248", "K59CLC", "tranhieu956230@gmail.com");
+        Student st2 = new Student("Trần Đức Hiếu", "17021248", "K59CLC", "tranhieu956230@gmail.com");
+        Student st3 = new Student("Trần Đức Hiếu", "17021248", "K59CB", "tranhieu956230@gmail.com");
 
         // print Student info
         System.out.println("Student's name is: " + st1.getName());
@@ -27,47 +29,76 @@ public class StudentManagement {
         st3.getInfo();
         System.out.println("----------------------------");
 
+
         // check for sameGroup
         System.out.println(sameGroup(st1, st2));
         System.out.println(sameGroup(st2, st3));
         System.out.println(sameGroup(st1, st3));
 
-        // 100 students
-
-        for (int i = 0; i < 50; i++) {
-            students[i] = new Student();
+        for (int i = 0; i < 10; i++) {
+            students.add(new Student("123", "123", "123", "123"));
+        }
+        for (int i = 10; i < 11; i++) {
+            students.add(new Student("1234", "1234", "1234", "1234"));
+        }
+        for (int i = 11; i < 30; i++) {
+            students.add(new Student("123", "123", "123", "123"));
+        }
+        for (int i = 30; i < 40; i++) {
+            students.add(new Student("1234", "1234", "1234", "1234"));
+        }
+        for (int i = 40; i < 50; i++) {
+            students.add(new Student("123", "123", "123", "123"));
+        }
+        for (int i = 50; i < 60; i++) {
+            students.add(new Student("1234", "1234", "1234", "1234"));
         }
 
-        for(int i = 50; i< students.length; i++) {
-            students[i] = new Student("Hieu", "1", "abc");
-        }
+
         // prints student by group
         studentsByGroup();
 
+        //remove Student
+        System.out.println("-------------------------------");
+        removeStudent("1234");
+        System.out.println("-------------------------------");
+
+        studentsByGroup();
+
+
     }
+
 
     public static boolean sameGroup(Student s1, Student s2) {
         return (s1.getGroup() == s2.getGroup());
     }
 
+
     public static void studentsByGroup() {
         Vector v = new Vector();
 
-        for (int i = 0; i < students.length; i++) {
-            if(v.indexOf(students[i].getGroup()) == -1) v.addElement(students[i].getGroup());
+        for (int i = 0; i < students.size(); i++) {
+            if (v.indexOf(students.get(i).getGroup()) == -1) v.addElement(students.get(i).getGroup());
             else continue;
-            for(int j = 0; j< students.length; j++) {
-                if(sameGroup(students[i], students[j])) {
-                    System.out.println(students[j].getName());
+            for (int j = 0; j < students.size(); j++) {
+                if (sameGroup(students.get(i), students.get(j))) {
+                    System.out.println(students.get(j).getName());
                 }
             }
         }
     }
 
+
     public static void removeStudent(String id) {
-        for(int i = 0;i < students.length; i++) {
-            if(students[i] )
+        for (int i = 0; i < students.size(); i++) {
+            if (students.get(i).getId() == id) {
+                students.remove(i);
+                i--;
+            }
         }
-        students = ArrayUtils.remove(students, )
     }
+
 }
+
+
+
